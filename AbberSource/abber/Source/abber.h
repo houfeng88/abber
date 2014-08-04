c99d5ebb49b148548e4f4e05a8f521fe924ec1b1
wuhp0415 <wuhp0415@thundersoft.com>
2014-08-04 18:10:23 +0800

    Set up structure

---
+++ AbberSource/abber/Source/abber.h
@@ -0,0 +1,14 @@
+//
+//  abber.h
+//  abber
+//
+//  Created by Kevin Wu on 8/4/14.
+//  Copyright (c) 2014 Tapmob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface abber : NSObject {
+}
+
+@end
---
+++ AbberSource/abber/Source/abber.m
@@ -0,0 +1,13 @@
+//
+//  abber.m
+//  abber
+//
+//  Created by Kevin Wu on 8/4/14.
+//  Copyright (c) 2014 Tapmob. All rights reserved.
+//
+
+#import "abber.h"
+
+@implementation abber
+
+@end
