eaadf984023480bee614d1d5eeed04d654857ee1
Kevin Wu <kvmisc@163.com>
2014-08-05 18:24:38 +0800

    Add log support

--- AbberDemo/Vendors/abber/ABLogger.m
+++ AbberDemo/Vendors/abber/ABLogger.m
@@ -9,12 +9,11 @@
 #import "ABLogger.h"
 #include <strophe/strophe.h>
 
+#ifdef DEBUG
 static const int ddLogLevel = LOG_LEVEL_DEBUG;
-//#ifdef DEBUG
-//
-//#else
-//static const int ddLogLevel = LOG_LEVEL_OFF;
-//#endif
+#else
+static const int ddLogLevel = LOG_LEVEL_OFF;
+#endif
 
 void ab_default_logger(void * const userdata,
                        const xmpp_log_level_t level,
@@ -24,13 +23,13 @@ void ab_default_logger(void * const userdata,
   xmpp_log_level_t filter_level = * (xmpp_log_level_t*)userdata;
   if (level >= filter_level) {
     if ( level==XMPP_LEVEL_DEBUG ) {
-      DDLogDebug(@"%s %s %s", area, "DEBUG", msg);
+      DDLogCError(@"%s %s %s", area, "DEBUG", msg);
     } else if ( level==XMPP_LEVEL_INFO ) {
-      DDLogInfo(@"%s %s %s", area, "INFO", msg);
+      DDLogCInfo(@"%s %s %s", area, "INFO", msg);
     } else if ( level==XMPP_LEVEL_WARN ) {
-      DDLogWarn(@"%s %s %s", area, "WARN", msg);
+      DDLogCWarn(@"%s %s %s", area, "WARN", msg);
     } else if ( level==XMPP_LEVEL_ERROR ) {
-      DDLogError(@"%s %s %s", area, "ERROR", msg);
+      DDLogCError(@"%s %s %s", area, "ERROR", msg);
     }
   }
 }
