08124bfe08b20fcf67b34bf46c6cd44938c46627
Kevin Wu <kvmisc@163.com>
2014-08-05 14:18:51 +0800

    blah blah

---
+++ AbberDemo/Vendors/abber/ABJid.h
@@ -0,0 +1,13 @@
+//
+//  ABJid.h
+//  AbberDemo
+//
+//  Created by Kevin Wu on 8/5/14.
+//  Copyright (c) 2014 Tapmob. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABJid : NSObject
+
+@end
---
+++ AbberDemo/Vendors/abber/ABJid.m
@@ -0,0 +1,13 @@
+//
+//  ABJid.m
+//  AbberDemo
+//
+//  Created by Kevin Wu on 8/5/14.
+//  Copyright (c) 2014 Tapmob. All rights reserved.
+//
+
+#import "ABJid.h"
+
+@implementation ABJid
+
+@end
