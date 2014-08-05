2839adbe1382514896e9be7393ed04a557bb9de0
wuhp0415 <wuhp0415@thundersoft.com>
2014-08-05 12:43:21 +0800

    Add demo project

---
+++ AbberDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AbberDemo
+//
+//  Created by Kevin Wu on 8/5/14.
+//  Copyright (c) 2014 Tapmob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder<
+    UIApplicationDelegate
+> {
+  UIWindow *_window;
+}
+
+@end
---
+++ AbberDemo/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  AbberDemo
+//
+//  Created by Kevin Wu on 8/5/14.
+//  Copyright (c) 2014 Tapmob. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  _window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  
+  _window.backgroundColor = [UIColor whiteColor];
+  [_window makeKeyAndVisible];
+  return YES;
+}
+
+@end
