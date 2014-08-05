e2b06a798d2bee0fba4f7f9df7d2b5e9eac617f3
Kevin Wu <kvmisc@163.com>
2014-08-05 16:04:57 +0800

    Add samples

--- AbberDemo/Vendors/abber/ABJid.h
+++ AbberDemo/Vendors/abber/ABJid.h
@@ -7,7 +7,9 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <strophe/strophe.h>
 
-@interface ABJid : NSObject
+@interface ABJid : NSObject {
+}
 
 @end
---
+++ AbberDemo/basic.h
@@ -0,0 +1,9 @@
+//
+//  basic.h
+//  StropheDemo
+//
+//  Created by Kevin Wu on 8/4/14.
+//  Copyright (c) 2014 Tapmob. All rights reserved.
+//
+
+void basic();
