08496b47a7f2a0c99b6dcb363196670cf0711df5
Kevin Wu <kvmisc@163.com>
2014-08-18 18:31:27 +0800

    blah blah

---
+++ AbberDemo/Sections/TKProfileViewController.h
@@ -0,0 +1,14 @@
+//
+//  TKProfileViewController.h
+//  AbberDemo
+//
+//  Created by Kevin Wu on 8/18/14.
+//  Copyright (c) 2014 Tapmob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKProfileViewController : TKViewController {
+}
+
+@end
---
+++ AbberDemo/Sections/TKProfileViewController.m
@@ -0,0 +1,18 @@
+//
+//  TKProfileViewController.m
+//  AbberDemo
+//
+//  Created by Kevin Wu on 8/18/14.
+//  Copyright (c) 2014 Tapmob. All rights reserved.
+//
+
+#import "TKProfileViewController.h"
+
+@implementation TKProfileViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+}
+
+@end
